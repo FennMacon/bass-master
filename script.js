b3dbc58240f3eb4d80e4e80edb2bca8c3ea3ab0d
Fenn Macon <fennmacon@gmail.com>
2016-01-28 22:33:59 -0500

    added .svg's

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 		$( "#piano" ).toggleClass("is-active");
 		$( "#pButton" ).toggleClass("btn-active");
 	});
+	$( "#pnButton" ).click(function(){
+		$( "#pianonotes" ).toggleClass("is-active");
+		$( "#pnButton" ).toggleClass("btn-active");
+	});
 	$( "#tcButton" ).click(function(){
 		$( "#tc" ).toggleClass("is-active");
 		$( "#tcButton" ).toggleClass("btn-active");
@@ -15,10 +19,18 @@ $(document).ready(function(){
 		$( "#full" ).toggleClass("is-active");
 		$( "#fcButton" ).toggleClass("btn-active");
 	});
+	$( "#bButton" ).click(function(){
+		$( "#b" ).toggleClass("is-active");
+		$( "#bButton" ).toggleClass("btn-active");
+	});	
 	$( "#btButton" ).click(function(){
 		$( "#bt" ).toggleClass("is-active");
 		$( "#btButton" ).toggleClass("btn-active");
-	});			
+	});		
+	$( "#bnButton" ).click(function(){
+		$( "#bn" ).toggleClass("is-active");
+		$( "#bnButton" ).toggleClass("btn-active");
+	});	
 	$( "#ndButton" ).click(function(){
 		$( "#nd" ).toggleClass("is-active");
 		$( "#ndButton" ).toggleClass("btn-active");
